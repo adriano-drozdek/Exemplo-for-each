a111075a8cfe73a416efbe797b1b40fc1adbfa6d
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-03-14 19:37:32 -0300

    Utilizando laço for each

--- src/forEach/LacoFor.java
+++ src/forEach/LacoFor.java
@@ -3,11 +3,17 @@ package forEach;
 public class LacoFor {
 
 	public static void main(String[] args) {
-		
-		String[] vect = new String[] {"Ana ", "Maria ", "João "};
-		
-		for (int i=0; i< vect.length; i++){
+
+		String[] vect = new String[] { "Ana ", "Maria ", "João " };
+
+		for (int i = 0; i < vect.length; i++) {
 			System.out.println(vect[i]);
+		}
+
+		System.out.println();
+
+		for (String obj : vect) {
+			System.out.println(obj);
 		}
 	}
 
