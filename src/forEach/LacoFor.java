a121eff510780e187fab36547e6bb7b31318b348
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-03-14 19:33:02 -0300

    Modo convencional

---
+++ src/forEach/LacoFor.java
@@ -0,0 +1,14 @@
+package forEach;
+
+public class LacoFor {
+
+	public static void main(String[] args) {
+		
+		String[] vect = new String[] {"Ana ", "Maria ", "João "};
+		
+		for (int i=0; i< vect.length; i++){
+			System.out.println(vect[i]);
+		}
+	}
+
+}
